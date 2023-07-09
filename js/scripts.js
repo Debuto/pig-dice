486255cd79a79df3472948d1236263e79e69f30c
Debo <dwdeante0726@gmail.com>
2023-07-09 10:48:51 -0700

    This will fix window event listener so roll button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
-window.addEventListener('load', function() {
-  document.addEventListener("DOMContentLoaded", function() {
-    const rollButton = document.getElementById("roll")
+window.addEventListener("DOMContentLoaded", function() {
+    const rollButton = document.getElementById("roll");
+    const outputDiv = document.getElementById("output");
+    
+    function rollDie() {
+      return Math.floor(Math.random() * 6) + 1;
+    } 
+
     rollButton.addEventListener("click", function() {
-      
-    })
-  }
-  }
-})
+      let result = rollDie();
+      outputDiv.innerText = "You rolled a " + result;
+    });
+});
