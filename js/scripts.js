85fb03f538e79672e6c1bef5d3db7a0e764f7b4c
Debo <dwdeante0726@gmail.com>
2023-07-09 16:42:06 -0700

    This will add gif background and logic for counting score.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 window.addEventListener("DOMContentLoaded", function() {
     const rollButton = document.getElementById("roll");
     const outputDiv = document.getElementById("output");
+    let score = 0; 
+    let countingScore = 0;
     
     function rollDie() {
       return Math.floor(Math.random() * 6) + 1;
@@ -8,6 +10,9 @@ window.addEventListener("DOMContentLoaded", function() {
 
     rollButton.addEventListener("click", function() {
       let result = rollDie();
-      outputDiv.innerText = "You rolled a " + result;
+      if (result === 1) {
+        countingScore = 0;
+      } else countingScore += result; 
+        outputDiv.innerText = "You rolled a " + result + ". Current score is " + countingScore;
     });
 });
