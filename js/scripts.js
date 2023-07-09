e4cc0bb84a9a7707010778fc3711c464ed721721
Debo <dwdeante0726@gmail.com>
2023-07-09 10:16:24 -0700

    This will add a roll button to html and js script logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 window.addEventListener('load', function() {
-  
+  document.addEventListener("DOMContentLoaded", function() {
+    const rollButton = document.getElementById("roll")
+    rollButton.addEventListener("click", function() {
+      
+    })
+  }
+  }
 })
